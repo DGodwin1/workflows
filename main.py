1c25ba66c7883e64cd85f33b8a666dffcc7f61f9
DGodwin1 <dgodwin26@yahoo.co.uk>
2025-12-05 14:57:08 +0000

    use unequivocal prefaces for functions called from outside main because we like readability

--- main.py
+++ main.py
@@ -2,16 +2,15 @@ import asyncio
 import os
 
 import orchestrator
-import participants
 
-from dotenv import load_dotenv
-from agent_framework.azure import AzureOpenAIChatClient
-from agent_framework.observability import setup_observability
+import dotenv
+import agent_framework.observability
+import agent_framework.azure
 
 
 
 async def main() -> None:
-    load_dotenv()
+    dotenv.load_dotenv()
     print("⚙️ Setting up domain agents and concurrent workflow")
 
     API_KEY = os.getenv("API_KEY")
@@ -19,18 +18,17 @@ async def main() -> None:
     DEPLOYMENT_NAME = os.getenv("DEPLOYMENT_NAME")
     WORKFLOW_TYPE = os.getenv("WORKFLOW_TYPE")
     if not API_KEY or not ENDPOINT or not DEPLOYMENT_NAME or not WORKFLOW_TYPE:
-        print("☠️ Something isn't set - check API_KEY, API_ENDPOINT, API_DEPLOYMENT_NAME and WORKFLOW_TYPE")
+        print("☠️ Something isn't set - check API_KEY, API_ENDPOINT, DEPLOYMENT_NAME and WORKFLOW_TYPE")
         return
 
     print("🔭 Setting up observability")
-    setup_observability()
+    agent_framework.observability.setup_observability()
 
-    chat_client = AzureOpenAIChatClient(api_key=API_KEY, endpoint=ENDPOINT, deployment_name=DEPLOYMENT_NAME)
-    legal, marketer, researcher = participants.create(chat_client)
+    chat_client = agent_framework.azure.AzureOpenAIChatClient(api_key=API_KEY, endpoint=ENDPOINT, deployment_name=DEPLOYMENT_NAME)
 
-    o = orchestrator.Orchestrator([legal, marketer, researcher], workflow=WORKFLOW_TYPE)
+    workflow_orchestrator = orchestrator.Orchestrator(chat_client=chat_client, workflow=WORKFLOW_TYPE)
 
-    await o.do_workflow()
+    await workflow_orchestrator.do_workflow()
 
 if __name__ == "__main__":
     asyncio.run(main())
